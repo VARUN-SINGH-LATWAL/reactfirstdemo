c117c9c7925207de676edfc8a58c65c1e63d57af
VARUN-SINGH-LATWAL <varunsingh0456@gmail.com>
2023-09-19 17:50:05 +0530

    first one

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>varun</h1>
+     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti temporibus nostrum delectus tenetur dolorem accusantium, nulla aspernatur molestias itaque id incidunt cupiditate impedit eligendi dolorum dignissimos enim ea architecto adipisci deserunt ipsum, praesentium dolores repellendus. Dolorem accusamus, mollitia ex voluptate eaque amet fugit saepe in minima rem quas aut debitis?</p>
+    </>
+  )
+}
+
+export default App
